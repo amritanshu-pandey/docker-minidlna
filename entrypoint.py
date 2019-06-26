40c46493cc1dcfb4118cd05e7899f791a482bfd0
Amritanshu Pandey <amp.msit@gmail.com>
2019-06-26 20:54:58 +1000

    allow multiple media dirs

--- entrypoint.py
+++ entrypoint.py
@@ -5,8 +5,13 @@ CONFIG_FILE = '/etc/minidlna'
 
 for each_env in os.environ:
     if each_env.startswith('cfg_'):
+        if each_env.startswith('cfg_media_dir'):
+            key = 'media_dir'
+        else:
+            key = each_env[4:]
+
         with open(CONFIG_FILE, 'a') as cfg:
-            cfg.write(f'{each_env[4:]}={os.environ[each_env]}')
+            cfg.write(f'{key}={os.environ[each_env]}')
 
 
 subprocess.call('/usr/sbin/minidlnad -d'.split())
