196ba65021aa86590d8ede8649a2042c0426ea74
Amritanshu Pandey <amp.msit@gmail.com>
2019-06-26 22:46:10 +1000

    delete minidlna config before entrypoint

--- entrypoint.py
+++ entrypoint.py
@@ -1,10 +1,9 @@
 import os
 import subprocess
-import shutil
 
 CONFIG_FILE = '/etc/minidlna'
 
-shutil.unlink(CONFIG_FILE)
+os.unlink(CONFIG_FILE)
 
 for each_env in os.environ:
     if each_env.startswith('cfg_'):
